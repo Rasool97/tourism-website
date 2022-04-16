c0acc97e3290a3d0e11a1ec24bfc637ecd6ce24c
Rasool97 <raybodjazini97@gmail.com>
2022-04-16 19:33:36 +0430

    build navbar and header

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    content: ["./src/**/*.html"],
+    theme: {
+        fontFamily: {
+            'vazir': ['Vazir'],
+        },
+        container: {
+            center: true,
+            padding: "1rem",
+        },
+        extend: {
+            padding: {
+                'md': "56%",
+            },
+            
+        },
+    },
+    plugins: [],
+};
