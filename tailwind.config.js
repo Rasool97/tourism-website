a99f8ed7364f7423a89b8b15c740db5b913a4ada
Rasool97 <raybodjazini97@gmail.com>
2022-04-22 11:29:48 +0430

    add most visited section & formd &  flex table & footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,8 +12,19 @@ module.exports = {
             padding: {
                 'md': "56%",
             },
-            
+             keyframes: {
+        wiggle: {
+          '0%, 100%': { transform: 'rotate(-7deg)' },
+          '50%': { transform: 'rotate(7deg)' },
+        }
+      },
+            animation: {
+                wiggle: 'wiggle 1s ease-in-out infinite'
+            }
         },
     },
-    plugins: [require('@tailwindcss/line-clamp'),],
+    plugins: [
+        require('@tailwindcss/line-clamp'),
+        require('@tailwindcss/forms'),
+    ],
 };
